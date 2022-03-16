d065234b295f941c7723922c61d110cbedda46b8
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-15 20:22:55 -0500

    Modified formatting

--- passport.js
+++ passport.js
@@ -7,22 +7,20 @@ const User = require('./models/user');
 const bcrypt = require('bcryptjs');
 
 // Local Strategy used for initial log in
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-        // Check if username & password matches in database
-        User.findOne({ username: username }, (err, user) => {
+// NOTE: "done" is an internal passport method, taking in the parameters (err, user, info)
+passport.use(new LocalStrategy((username, password, done) => {
+    // Check if username & password matches in database
+    User.findOne({ username: username }, (err, user) => {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Incorrect username.' }); }
+        bcrypt.compare(password, user.password, (err, res) => {
             if (err) { return done(err); }
-            if (!user) { return done(null, false, { message: 'Incorrect username.' }); }
-            bcrypt.compare(password, user.password, (err, res) => {
-                if (err) { return done(err); }
-                if (res) { return done(null, user, { message: 'Logged in successfully.' }); }
-                else { return done(null, false, { message: 'Incorrect password.' }); }
-            });
+            if (res) { return done(null, user, { message: 'Logged in successfully.' }); }
+            else { return done(null, false, { message: 'Incorrect password.' }); }
         });
-    }
-));
+    });
+}));
 
-// Jwt Options
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extracts token from authorization header with the scheme "bearer"
     secretOrKey: 'jwt_secret' // The secret (symmetric) or PEM-encoded public key (asymmetric) for verifying the token's signature
