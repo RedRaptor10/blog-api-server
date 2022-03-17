41c79c4f517c229aee7a054df349e113fef5eed3
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-17 04:28:21 -0500

    Moved login function to indexController & added form validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,36 @@
-// Index
-exports.index = function(req, res, next) {
-    res.json({
-        test: 'test'
-    });
-};
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+// Log In
+exports.logIn = [
+    // Validate and sanitize fields
+    body('username', 'Username required.').trim().isLength({ min: 1 }).escape(),
+    body('password', 'Password required.').trim().isLength({ min: 1 }).escape(),
+
+    // Process Log In
+    (req, res, next) => {
+        // Extract the validation errors from request
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Error, render form again with sanitized values/error messages
+            res.json({ username: req.body.username, errors: errors.array() });
+        } else {
+            // Authenticate with database (NOTE: {session: false} is passed so that the user isn't saved in the session.)
+            passport.authenticate('local', {session: false}, (error, user, info) => {
+	        if (error || !user) {
+	            return res.status(400).json({ user, error, info });
+	        }
+
+	        // Log the user in
+	        req.login(user, {session: false}, (error) => {
+	            if (error) { res.json(error); }
+	            // Generate a signed JSON web token with the contents of the user object (NOTE: Access user info via req.user.info)
+	            const token = jwt.sign({info: user}, 'jwt_secret', { expiresIn: '60s' });
+	            return res.json({ user, token });
+	        });
+            })(req, res);
+        }
+    }
+];
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
 
 // Controller
 const indexController = require('../controllers/indexController');
 
-// Index
-router.get('/', indexController.index);
-
 // Log In
-router.post('/login', function (req, res, next) {
-    // Authenticate with database (NOTE: {session: false} is passed so that the user isn't saved in the session.)
-    passport.authenticate('local', {session: false}, (err, user, info) => {
-        if (err || !user) {
-            return res.status(400).json({
-                user: user,
-                error: err,
-                info
-            });
-        }
-
-        // Log the user in
-        req.login(user, {session: false}, (err) => {
-            if (err) { res.json(err); }
-            // Generate a signed JSON web token with the contents of the user object (NOTE: Access user info via req.user.info)
-            const token = jwt.sign({info: user}, 'jwt_secret', { expiresIn: '60s' });
-            return res.json({ user, token });
-        });
-    })(req, res);
-});
+router.post('/login', indexController.logIn);
 
 // Test (Should only be accessible if logged in)
-router.get('/test', passport.authenticate('jwt', {session: false}), (req, res) => { res.json('Accessible route as you are logged in.')});
+//router.get('/test', passport.authenticate('jwt', {session: false}), (req, res) => { res.json('Accessible route as you are logged in.')});
 
 module.exports = router;
