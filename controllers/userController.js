1b61ddcd9af132ba90d0d14f2af356a002224fb6
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-18 19:06:26 -0500

    Added update user form validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -92,44 +92,58 @@ exports.createUser = [
 
 // Update User
 exports.updateUser = [
+    // Validate and sanitize fields
+    body('username').trim().isLength({ min: 1 }).escape().withMessage('Username required.')
+        .isLength({ max: 20 }).withMessage('Username must have 20 characters or less.'),
+    body('password', 'Password must contain at least 5 characters.').trim().isLength({ min: 5 }).escape(),
+    body('confirmPassword', 'Passwords do not match.').trim().escape().custom((value, { req }) => value === req.body.password),
+
+    // Process Sign Up
     (req, res, next) => {
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password,
-            role: req.body.role
-        });
+        // Extract the validation errors from request
+        const errors = validationResult(req);
 
-        // Check if username already exists AND is not same as previous username
-        User.findOne({ 'username': user.username })
-        .exec(function(err, results) {
-            if (err) { return next(err); }
-            else if (results && results.username != req.params.username) {
-                res.json({ message: 'Username already exists.' });
-            } else {
-                // Hash password
-                bcrypt.hash(user.password, 10, (err, hashedPassword) => {
-                    if (err) { return next(err); }
-                    user.password = hashedPassword;
+        if (!errors.isEmpty()) {
+            res.json({ username: req.body.username, errors: errors.array() });
+        } else {
+            const user = new User({
+                username: req.body.username,
+                password: req.body.password,
+                role: req.body.role
+            });
 
-                    // Update user info from database
-                    User.findOneAndUpdate(
-                        { 'username': req.params.username }, // Filter
-                        { 'username': user.username, 'password': user.password, 'role': user.role }, // New values
-                        function(err) {
-                            if (err) { return next(err); }
-                            res.json({
-                                user: {
-                                    'id': user._id,
-                                    username: user.username,
-                                    role: user.role
-                                },
-                                message: 'Success'
-                            });
-                        }
-                    );
-                });
-            }
-        });
+            // Check if username already exists AND is not same as previous username
+            User.findOne({ 'username': user.username })
+            .exec(function(err, results) {
+                if (err) { return next(err); }
+                else if (results && results.username != req.params.username) {
+                    res.json({ message: 'Username already exists.' });
+                } else {
+                    // Hash password
+                    bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+                        if (err) { return next(err); }
+                        user.password = hashedPassword;
+
+                        // Update user info from database
+                        User.findOneAndUpdate(
+                            { 'username': req.params.username }, // Filter
+                            { 'username': user.username, 'password': user.password, 'role': user.role }, // New values
+                            function(err) {
+                                if (err) { return next(err); }
+                                res.json({
+                                    user: {
+                                        'id': user._id,
+                                        username: user.username,
+                                        role: user.role
+                                    },
+                                    message: 'Success'
+                                });
+                            }
+                        );
+                    });
+                }
+            });
+        }
     }
 ];
 
